43e8bc81d054c10b269af3594c9a80b19d4d1d65
24DP1AVasi <a240731av@rvt.lv>
2026-04-02 10:14:15 +0300

    funkcija edit profile izveidota

--- python_backend/conditions.py
+++ python_backend/conditions.py
@@ -1,14 +1,11 @@
 from datetime import datetime
-from time import sleep
 
 
 def string_to_date(send_date: str) -> datetime.date:
     """
     funkcija <string_to_date>
-    pieņem <str>
-    tipa vērtību <send_date>
-    un atgriež <datetime.date>
-    tipa vērtību <date>
+    pieņem <str> tipa vērtību <send_date>
+    un atgriež <datetime.date> tipa vērtību <date>
     """
     date = datetime.strptime(send_date, "%Y-%m-%d").date()
     return date
@@ -16,10 +13,8 @@ def string_to_date(send_date: str) -> datetime.date:
 def string_to_time(send_time: str) -> datetime.time:
     """
     funkcija <string_to_time>
-    pieņem <str>
-    tipa vērtību <send_time>
-    un atgriež <datetime.time>
-    tipa vērtību <time>
+    pieņem <str> tipa vērtību <send_time>
+    un atgriež <datetime.time> tipa vērtību <time>
     """
     time = datetime.strptime(send_time, "%H:%M").time()
     return time
@@ -27,10 +22,8 @@ def string_to_time(send_time: str) -> datetime.time:
 def combined_date_time(send_date: str, send_time: str) -> datetime:
     """
     funkcija <string_to_date, string_to_time>
-    pieņem <str, str>
-    tipa vērtību <send_date, send_time>
-    un atgriež <datetime>
-    tipa vērtību <date>
+    pieņem <str, str> tipa vērtību <send_date, send_time>
+    un atgriež <datetime> tipa vērtību <date>
     """
     date = datetime.strptime(f"{send_date} {send_time}", "%Y-%m-%d %H:%M")
     return date
@@ -38,10 +31,8 @@ def combined_date_time(send_date: str, send_time: str) -> datetime:
 def send_email_on(send_date: str, send_time: str) -> bool:
     """
     funkcija <send_email_on>
-    pieņem <str, str>
-    tipa vērtību <send_date, send_time>
-    un atgriež <bool>
-    tipa vērtību <bool>
+    pieņem <str, str> tipa vērtību <send_date, send_time>
+    un atgriež <bool> tipa vērtību <bool>
     """
     if combined_date_time(send_date, send_time) == datetime.now():
         return True
@@ -51,10 +42,8 @@ def send_email_on(send_date: str, send_time: str) -> bool:
 def send_email_until(send_date: str, send_time: str) -> bool:
     """
     funkcija <send_email_until>
-    pieņem <str, str>
-    tipa vērtību <send_date, send_time>
-    un atgriež <bool>
-    tipa vērtību <bool>
+    pieņem <str, str> tipa vērtību <send_date, send_time>
+    un atgriež <bool> tipa vērtību <bool>
     """
     if combined_date_time(send_date, send_time) > datetime.now():
         return True
--- python_backend/profiles.py
+++ python_backend/profiles.py
@@ -2,18 +2,18 @@ import json
 import uuid
 
 
-def create_profile(sender_email: str, receiver_emails: list[str], password: str, topic: str, email_content: str, condition: int) -> dict[str, dict[str: list[str] | str | int]]:
+def create_profile(sender_email: str, name: str, receiver_emails: list[str], description: str, password: str, topic: str, email_content: str, condition: int) -> dict[str, dict[str: list[str] | str | int]]:
     """
     Funkcija <create_profile>
-    pieņem <list[str], list[str], str, str, str, int>
-    tipa vērtību <sender_email, receiver_email, password, topic, email_content, condition>
-    un atgriež <dict[str, dict[str: list[str] | str | int]]>
-    tipa vērtību <new_profile>
+    pieņem <list[str], list[str], str, str, str, str, str, int> tipa vērtību <sender_email, name, receiver_email, description, password, topic, email_content, condition>
+    un atgriež <dict[str, dict[str: list[str] | str | int]]> tipa vērtību <new_profile>
     """ 
     new_profile = {
         str(uuid.uuid1()): {
             "sender_email": sender_email,
+            "name": name,
             "receiver_emails": receiver_emails,
+            "description": description,
             "password": password,
             "topic": topic,
             "email_content": email_content,
@@ -27,6 +27,27 @@ def create_profile(sender_email: str, receiver_emails: list[str], password: str,
         profiles = {}
     profiles.update(new_profile)
     with open(".profiles.json", "w") as f:
-        json.dump(profiles, f)
+        json.dump(profiles, f, indent=4)
 
     return new_profile
+
+def edit_profile(name: str, description: str, updates: dict):
+    """
+    """
+    try:
+        with open(".profiles.json", "r") as f:
+            profiles = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return None
+
+    for profile_id, profile_data in profiles.items():
+        if profile_data.get("name") == name and profile_data.get("description") == description:
+            for key, value in updates.items():
+                if value is not None:
+                    profile_data[key] = value
+            with open(".profiles.json", "w") as f:
+                json.dump(profiles, f, indent=4)
+
+            return {profile_id: profile_data}
+
+    return None
