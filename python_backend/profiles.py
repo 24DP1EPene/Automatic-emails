86cb43c7fd55b6ad19a83e03c186595b693c7078
24DP1AVasi <a240731av@rvt.lv>
2026-04-02 10:37:55 +0300

    izveidota funkcija delete profile

--- python_backend/profiles.py
+++ python_backend/profiles.py
@@ -5,7 +5,7 @@ import uuid
 def create_profile(sender_email: str, name: str, receiver_emails: list[str], description: str, password: str, topic: str, email_content: str, condition: int) -> dict[str, dict[str: list[str] | str | int]]:
     """
     Funkcija <create_profile>
-    pieņem <list[str], list[str], str, str, str, str, str, int> tipa vērtību <sender_email, name, receiver_email, description, password, topic, email_content, condition>
+    pieņem <str, list[str], str, str, str, str, str, int> tipa vērtību <sender_email, name, receiver_email, description, password, topic, email_content, condition>
     un atgriež <dict[str, dict[str: list[str] | str | int]]> tipa vērtību <new_profile>
     """ 
     new_profile = {
@@ -33,12 +33,15 @@ def create_profile(sender_email: str, name: str, receiver_emails: list[str], des
 
 def edit_profile(name: str, description: str, updates: dict):
     """
+    Funkcija <edit_profile>
+    pieņem <str, str, dict[str, dict[str: list[str] | str | int]]> tipa vērtību <>
+    un atgriež <> tipa vērtību <>
     """
     try:
         with open(".profiles.json", "r") as f:
             profiles = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
-        return None
+        return
 
     for profile_id, profile_data in profiles.items():
         if profile_data.get("name") == name and profile_data.get("description") == description:
@@ -50,4 +53,20 @@ def edit_profile(name: str, description: str, updates: dict):
 
             return {profile_id: profile_data}
 
-    return None
+def delete_profile(name: str, description: str):
+    try:
+        with open(".profiles.json", "r") as f:
+            profiles = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return
+    
+    profile_to_delete = None
+    for profile_id, profile_data in profiles.items():
+        if profile_data.get("name") == name and profile_data.get("description") == description:
+            profile_to_delete = profile_id
+            break
+    if profile_to_delete is None:
+        return
+    del profiles[profile_to_delete]
+    with open(".profiles.json", "w") as f:
+        json.dump(profiles, f, indent=4)
